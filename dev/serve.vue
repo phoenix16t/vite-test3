4d22f1d39480a1f09ff8e4841ef080822a4665ba
Eddie Jimenez <jimenez.eddie@gmail.com>
2021-04-12 20:20:21 -0700

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,19 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { TestlibSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  TestlibSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <HelloWorld msg="in serve" />
+    <testlib-sample />
+  </div>
+</template>
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/prefer-default-export */
+// export { default as TestlibSample } from './testlib-sample.vue';
+// export { default as HelloWorld } from './HelloWorld.vue';
+
+
+import TestlibSample from './testlib-sample.vue';
+import HelloWorld from './HelloWorld.vue';
+export { HelloWorld, TestlibSample };
